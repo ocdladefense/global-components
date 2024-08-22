7765bf744474ea4bb97cfc2ed20462e555a43991
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-08-22 13:20:04 -0700

    Some more minor cleanup.

--- src/Breadcrumbs.jsx
+++ src/Breadcrumbs.jsx
@@ -4,7 +4,7 @@ import { vNode } from '@ocdla/view';
 import Breadcrumbs_Item from './Breadcrumbs_Item';
 /* eslint-enable */
 
-export default function Breadcrumbs({ crumbs }) {
+export default function Breadcrumbs({ crumbs = [] }) {
     return (
         <section class='flex items-center border border-t-0 p-4 text-black lg:h-16'>
             <ul class='flex flex-wrap items-center whitespace-pre'>
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,7 +5,7 @@ import Sidebar_Item_Left from '@ocdla/global-components/src/Sidebar_Item_Left';
 import Sidebar_Item_Right from '@ocdla/global-components/src/Sidebar_Item_Right';
 /* eslint-enable */
 
-export default function Sidebar({ type, items }) {
+export default function Sidebar({ type, items = [] }) {
     return (
         <aside class='hidden h-[87.5vh] overflow-scroll lg:block'>
             <ul>
