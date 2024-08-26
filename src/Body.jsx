a1294afde9be65b3ad7b2092f69b017794c6b269
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-08-25 18:28:03 -0700

    Implemented basic spacing between elements.

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,13 +2,12 @@
 /* eslint-disable-next-line no-unused-vars */
 import { vNode } from '@ocdla/view';
 
-export default function Body({ body }) {
-    // console.log(body);
-
+export default function Body({ body, typeOrs }) {
     return (
+        // prettier-ignore
         <section
             id='body'
-            class='flex w-full flex-col gap-4 p-4 lg:col-span-4 lg:col-start-2 lg:me-auto lg:border-x lg:p-8'>
+            class={`${typeOrs ? '[&_*]:mb-4 ' : ''}flex w-full flex-col gap-4 p-4 lg:col-span-4 lg:col-start-2 lg:me-auto lg:border-x lg:p-8`}>
             {body}
         </section>
     );
