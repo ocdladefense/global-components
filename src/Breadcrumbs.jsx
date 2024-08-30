ba384491454603fcb768e91f1d01bcb10511734a
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-08-30 11:07:26 -0700

    Capitalize first letter of each breadcrumb item.

--- src/Breadcrumbs.jsx
+++ src/Breadcrumbs.jsx
@@ -6,7 +6,7 @@ import BreadcrumbItem from './BreadcrumbItem';
 
 export default function Breadcrumbs({ crumbs = [] }) {
     return (
-        <section class='flex items-center border border-t-0 p-4 text-black lg:h-16'>
+        <section class='flex items-center border border-t-0 p-4 capitalize text-black lg:h-16'>
             <ul class='flex flex-wrap items-center whitespace-pre'>
                 {crumbs.map((crumb, i) => {
                     const seperatorString =
