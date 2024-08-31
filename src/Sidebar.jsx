c5356535f71b21af6cd5f2dd7090341455baa020
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2024-08-31 13:06:17 -0700

    Reformatted Prettier comments.

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,7 +4,7 @@ import { vNode } from '@ocdla/view';
 
 export default function Sidebar({ children, id, sticky = false }) {
     return (
-        // prettier-ignore
+        /* prettier-ignore */
         <aside
             id={id || null}
             class={`${sticky ? 'lg:sticky lg:top-0 ' : ''}hidden h-[87.5vh] list-none overflow-y-scroll lg:block`}>
