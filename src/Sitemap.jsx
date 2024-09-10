45260e2ec436e2779e4c6ed5132f40b08e110b4e
SullivanKE <Katelyn135@gmail.com>
2024-09-10 12:11:30 -0700

    Changed format of Sitemap to be easier to use. It now has a category component that makes it easier to write directly in code and read.

--- src/Sitemap.jsx
+++ src/Sitemap.jsx
@@ -1,29 +1,16 @@
 /** @jsx vNode */
 import { vNode } from "@ocdla/view";
-import Link from "@ocdla/global-components/src/Defaults";
 
 // <li class="size-full"> This was wrapping the entire return. We may need to keep size-full when using this.
 // [{title: "SERVICES", links: [{href="/", label: "Membership Directory"}, {} ]}, {}]
-export default function Sitemap({ categories }) {
+export default function Sitemap({ className, children }) {
     return (
-        <ul class="flex flex-col gap-8 text-nowrap text-[#516490] lg:flex-row lg:gap-16">
-            {/* Categories */}
-            {categories.map(category => (
-                <li>
-                    <ul class="flex flex-col gap-1">
-                        {/* Category title */}
-                        <li>
-                            <p class="text-base font-bold">{category.title}</p>
-                        </li>
-                        {/* Links in each category */}
-                        {category.links.map(link => (
-                            <li>
-                                <Link href={link.href}>{link.label}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </li>
-            ))}
+        <ul
+            class={
+                className ||
+                "flex flex-col gap-8 text-nowrap text-[#516490] lg:flex-row lg:gap-16"
+            }>
+            {children}
         </ul>
     );
 }
---
+++ src/SitemapCategory.jsx
@@ -0,0 +1,36 @@
+/** @jsx vNode */
+import { vNode } from "@ocdla/view";
+import Link from "@ocdla/global-components/src/Defaults";
+
+// <li class="size-full"> This was wrapping the entire return. We may need to keep size-full when using this.
+// [{title: "SERVICES", links: [{href="/", label: "Membership Directory"}, {} ]}, {}]
+export default function SitemapCategory({ title, path, labels, hrefs }) {
+    // Split our labels and hrefs into arrays
+    labels = labels.replace(" ", "").split(",");
+    hrefs = hrefs.replace(" ", "").split(",");
+
+    // Remove trailing '/' if there is one.
+    path = path.at(-1) == "/" ? path.slice(0, -1) : path;
+    return (
+        <ul class="flex flex-col gap-8 text-nowrap text-[#516490] lg:flex-row lg:gap-16">
+            <li>
+                <ul class="flex flex-col gap-1">
+                    {/* Category title */}
+                    <li>
+                        <p class="text-base font-bold">
+                            <Link href="{path}">{title}</Link>
+                        </p>
+                    </li>
+                    {/* Links in each category */}
+                    {hrefs.map((href, i) => (
+                        <li>
+                            <Link href={href[0] == "/" ? path + href : href}>
+                                {labels[i]}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </li>
+        </ul>
+    );
+}
