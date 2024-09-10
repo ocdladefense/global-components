6d3f31cb347b35fe0bc42efe8fb824f2cb293931
SullivanKE <Katelyn135@gmail.com>
2024-09-10 14:50:02 -0700

    Testing with ContentRegion has shown columns to be fairly easy to make. Added a few attributes to assist with columns and gaps between them.

--- src/ContentRegion.jsx
+++ src/ContentRegion.jsx
@@ -2,6 +2,6 @@
 /* eslint-disable-next-line no-unused-vars */
 import { vNode } from "@ocdla/view";
 
-export default function ContentRegion({ cols, children }) {
-  return <section class={`columns-${cols}`}>{children}</section>;
+export default function ContentRegion({ cols, gap, children }) {
+    return <section class={`gap-x-${gap || "0"} columns-${cols || "1"} mb-8`}>{children}</section>;
 }
