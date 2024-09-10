111f95a63663fb2c099e3bc14ce17459adda8cc1
SullivanKE <Katelyn135@gmail.com>
2024-09-10 11:54:36 -0700

    Modified Legal to have a year attribute and an association attribute; Changed the usage of Social to now accept a stringed list of platforms and handles.

--- src/Legal.jsx
+++ src/Legal.jsx
@@ -2,11 +2,17 @@
 /* eslint-disable-next-line no-unused-vars */
 import { vNode } from "@ocdla/view";
 
-export default function Legal({ className, copyright, children }) {
-  return (
-    <ul class={className}>
-      <li>{copyright || ""}</li>
-      <li class="size-full text-wrap">{children || ""}</li>
-    </ul>
-  );
+export default function Legal({ className, year, association, children }) {
+    return (
+        <ul
+            class={
+                className ||
+                "text-neutral-500 text-[0.625rem] font-thin leading-[0.75rem]"
+            }>
+            <li>
+                ©{year} {association}
+            </li>
+            <li class="size-full text-wrap">{children || ""}</li>
+        </ul>
+    );
 }
--- src/Social.jsx
+++ src/Social.jsx
@@ -1,64 +1,20 @@
 /** @jsx vNode */
 /* eslint-disable no-unused-vars */
-import { vNode } from '@ocdla/view';
-import Link from './Defaults';
+import { vNode } from "@ocdla/view";
+import SocialItem from "./SocialItem";
 /* eslint-enable */
-// import abc from './images';
-import logo_facebook from './images/logo_facebook.png';
-import logo_twitter from './images/logo_twitter.png';
-import logo_youtube from './images/logo_youtube.png';
 
-export default function Social({ type, handle, src }) {
-    // require.context('./', true, /\.(svg|png)$/gim);
-
-    let domain;
-    let alt;
-
-    handle = handle || '';
-
-    // console.log(abc);
-
-    switch (type) {
-        case 'facebook':
-        case 'meta':
-            domain = 'https://facebook.com/';
-            src = src || logo_facebook;
-            alt = 'Facebook logo';
-            break;
-        case 'twitter':
-        case 'x':
-            domain = 'https://x.com/';
-            src = src || logo_twitter;
-            alt = 'Twitter logo';
-            break;
-        case 'youtube':
-            domain = 'https://youtube.com/@';
-            // Temp
-            src = src || logo_youtube;
-            alt = 'YouTube logo';
-            break;
-        case 'reddit':
-            domain = 'https://reddit.com/r/';
-            // TBD
-            src = src || logo_twitter;
-            alt = 'Reddit logo';
-            break;
-    }
-
-    const href = domain + handle;
-    // const src = './images/' + type + '.png';
-
+export default function Social({ platforms, handles }) {
+    platforms = platforms.replaceAll(" ", "").split(",");
+    handles = handles.replaceAll(" ", "").split(",");
     return (
-        <li>
-            <Link
-                classes='hover:opacity-[67.5%]'
-                href={href}>
-                <img
-                    class='w-8'
-                    src={src}
-                    alt={alt}
+        <ul class="flex items-center gap-1">
+            {platforms.map((platform, i) => (
+                <SocialItem
+                    type={platform}
+                    handle={handles[i]}
                 />
-            </Link>
-        </li>
+            ))}
+        </ul>
     );
 }
