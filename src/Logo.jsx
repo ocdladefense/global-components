98784ff2458c756350f7056f9570df5249dbd000
SullivanKE <Katelyn135@gmail.com>
2024-09-17 09:04:50 -0700

    Changed how Logo works so that it is no longer wrapped in an li tag.

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -2,21 +2,17 @@
 /* eslint-disable-next-line no-unused-vars */
 import { vNode } from '@ocdla/view';
 
-export default function Logo({ typeNavbar, src }) {
+export default function Logo({ size, src }) {
     // Default = 'footer'
-    const li = typeNavbar ? 'size-full' : '';
-    const a = typeNavbar ? 'flex px-4' : '';
+    const a = size ? 'flex px-4' : '';
 
     return (
-        <li class={li}>
             <a
                 class={a}
                 href='/'>
                 <img
-                    class='h-16'
                     src={src}
                 />
             </a>
-        </li>
-    );
+  );
 }
