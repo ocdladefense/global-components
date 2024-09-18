50ad3252bf9c94d110ca9f733312c1cbef571684
SullivanKE <Katelyn135@gmail.com>
2024-09-18 13:50:03 -0700

    Removed li arom around the content of GoogleMap component so that it can be used outside of a list.

--- src/GoogleMaps.jsx
+++ src/GoogleMaps.jsx
@@ -4,14 +4,10 @@ import { vNode } from '@ocdla/view';
 
 export default function GoogleMaps({ src }) {
     return (
-        <li>
-            <ul>
                 <iframe
                     class='aspect-square w-full border-0 lg:w-64'
                     src={src}
                     allowfullscreen
                     referrerpolicy='no-referrer-when-downgrade'></iframe>
-            </ul>
-        </li>
     );
 }
